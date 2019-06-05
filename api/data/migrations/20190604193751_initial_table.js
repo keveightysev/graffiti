857263c6172167c4bb5d3c14503b41fba40c446d
keveightysev <keveightysev@gmail.com>
2019-06-04 19:56:56 -0500

    basic server set up

---
+++ api/data/migrations/20190604193751_initial_table.js
@@ -0,0 +1,9 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('blob', table => {
+    table.blob('blob');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('blob');
+};
---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send(`
+        <h1>This is my server</h1>
+        <h2>There are others like it but this one is mine</h2>
+    `);
+});
+
+module.exports = server;
