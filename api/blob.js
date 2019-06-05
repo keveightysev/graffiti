7486df5a73fa9fd80efc599e6a89257e7c26de9b
keveightysev <keveightysev@gmail.com>
2019-06-04 20:13:18 -0500

    Basic route for blob for sqlite

---
+++ api/blob.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+const db = require('./data/dbConfig.js');
+
+const get = () => {
+  return db('blob').first();
+};
+
+const add = async blob => {
+  return await db('blob').insert(blob);
+};
+
+const update = async blob => {
+  return await db('blob')
+    .where({ id: 1 })
+    .update(blob);
+};
+
+router.post('/new', async (req, res) => {
+  try {
+    await add(req.body);
+    res.status(201).end();
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const blob = await get();
+    res.status(200).send(blob);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.put('/update', async (req, res) => {
+  const blob = await update(req.body);
+  res.status(200).end();
+});
--- api/data/migrations/20190604193751_initial_table.js
+++ api/data/migrations/20190604193751_initial_table.js
@@ -1,5 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable('blob', table => {
+    table.increments();
     table.blob('blob');
   });
 };
